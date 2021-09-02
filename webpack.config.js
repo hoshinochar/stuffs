4c6a6460d665eb248368c03ebc62e725fefe0eff
char <hoshinochar@gmail.com>
2021-09-02 14:01:46 -0700

    vuetify

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// webpack.config.js
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            // Requires sass-loader@^7.0.0
+            options: {
+              implementation: require('sass'),
+              indentedSyntax: true // optional
+            },
+            // Requires sass-loader@^8.0.0
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                indentedSyntax: true // optional
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ['babel-loader', 'vue-svg-loader'],
+        options: {
+          svgo: {
+            plugins: [
+              {removeDoctype: true},
+              {removeComments: true},
+              {cleanupIDs: false},
+              {collapseGroups: false},
+              {removeEmptyContainers: false}
+            ]
+          }
+        }
+      },
+    ],
+  }
+}
