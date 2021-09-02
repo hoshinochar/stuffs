905e373c544e890b4fb0677a3d5217b639e834f9
char <hoshinochar@gmail.com>
2021-09-02 14:23:34 -0700

    router

---
+++ src/views/DeletedAffair.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>char beep boop</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'DeletedAffair',
+}
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,8 @@ module.exports = {
             // Requires sass-loader@^7.0.0
             options: {
               implementation: require('sass'),
-              indentedSyntax: true // optional
-            },
-            // Requires sass-loader@^8.0.0
-            options: {
-              implementation: require('sass'),
+              indentedSyntax: true, // optional
+              // Requires sass-loader@^8.0.0
               sassOptions: {
                 indentedSyntax: true // optional
               },
